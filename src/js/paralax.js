367bc3405e2e4aa728709c0055301787f195f2f5
Emerson Almeida <emerson.almeida@adviseu.com.br>
2023-05-24 13:25:06 -0300

    Adding responsiveness to the menu and who i am card

--- src/js/paralax.js
+++ src/js/paralax.js
@@ -22,15 +22,3 @@ window.onload = function() {
   })
   
 }
-
-const btnMobile = document.getElementById("btnMobile");
-
-function menu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-}
-
-function menuLateral() {
-    const menuLateral = document.getElementById("menuLateral");
-    menuLateral.classList.toggle('active');
-}
